a249be89e7b61e0c4f6bc8802c616eb7d32b21c5
Gundi <goncalo.neto@webdetails.pt>
2017-07-11 18:08:54 +0100

    Adds BetterTouchTool, Slack and Visual Studio Code

--- etc/cask.sh
+++ etc/cask.sh
@@ -8,7 +8,7 @@ brew cask install google-chrome
 brew cask install firefox
 brew cask install macvim
 brew cask install franz
-# brew cask install visual-studio-code
+brew cask install visual-studio-code
 brew cask install sourcetree
 brew cask install spotify
 brew cask install github-desktop
@@ -18,3 +18,5 @@ brew cask install intellij-idea
 brew cask install postgres
 brew cask install atom
 # brew cask install microsoft-office
+brew cask install slack
+brew cask install bettertouchtool
